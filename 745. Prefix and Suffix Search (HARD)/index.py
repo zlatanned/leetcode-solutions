5c63e6ca7ba4a062c63ff84ba2e67bc8028a5cf5
zlatanned <shahiakshay31@gmail.com>
2021-05-01 17:22:10 +0530

    added problem sample explanation in comments

--- 745. Prefix and Suffix Search (HARD)/index.py
+++ 745. Prefix and Suffix Search (HARD)/index.py
@@ -6,6 +6,19 @@ WordFilter(string[] words) Initializes the object with the words in the dictiona
 f(string prefix, string suffix) Returns the index of the word in the dictionary which has the prefix prefix and the suffix suffix.
 If there is more than one valid index, return the largest of them.
 If there is no such word in the dictionary, return -1.
+
+Example 1:
+
+Input:
+["WordFilter", "f"]
+[[["apple"]], ["a", "e"]]
+Output:
+[null, 0]
+
+Explanation:
+WordFilter wordFilter = new WordFilter(["apple"]);
+wordFilter.f("a", "e"); // return 0, because the word at index 0 has prefix = "a" and suffix = 'e".
+
 """
 
 class TrieNode:
