bc72be69f8955d58ca05180fd09b8a12909a199c
Akshay Shahi <shahiakshay31@gmail.com>
2021-05-11 22:35:52 +0530

    solved sliding window type problem

---
+++ 1423. Maximum Points You Can Obtain from Cards/index.js
@@ -0,0 +1,21 @@
+var maxScore = function(cardPoints, k) {
+  let max = 0
+  // calculate the initial sum
+  let i = 0
+  while (i < k){
+        max += cardPoints[i]
+      i++
+  }
+  
+  // shift it backwards 1 position for k times and update the max
+  let offset = 1
+  let sum = max
+  while(offset <= k){
+    sum = sum - cardPoints[k - offset] + cardPoints[cardPoints.length - offset]
+    max = Math.max(max, sum)
+    offset++
+  }
+  
+  return max
+  
+};
