6ae7454c09cfd60d0f4c48bc99771803a64722b0
Akshay Shahi <shahiakshay31@gmail.com>
2021-05-26 23:30:08 +0530

    fixed using string.charAt

--- 1281. Subtract the Product and Sum of Digits of an Integer/zlatanned.js
+++ 1281. Subtract the Product and Sum of Digits of an Integer/zlatanned.js
@@ -2,11 +2,10 @@ const diffProductSum = (n) => {
     let sum = 0;
     let product = 1;
     let num = n + '';
-    for (const x in n){
-        digit = int(x);
+    for (const i in num){
+        digit = parseInt(num.charAt(i));
         sum += digit;
         product *= digit;
     }
-    
     return (product - sum);
 }
