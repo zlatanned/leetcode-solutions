f3bc3dc97845b0037dc9cfa814aee424ca91670e
Akshay Shahi <shahiakshay31@gmail.com>
2021-05-26 23:14:52 +0530

    corrected for in syntax

--- 1281. Subtract the Product and Sum of Digits of an Integer/zlatanned.js
+++ 1281. Subtract the Product and Sum of Digits of an Integer/zlatanned.js
@@ -2,8 +2,8 @@ const diffProductSum = (n) => {
     let sum = 0;
     let product = 1;
     let num = n + '';
-    for x in n{
-        digit = int(n);
+    for (const x in n){
+        digit = int(x);
         sum += digit;
         product *= digit;
     }
