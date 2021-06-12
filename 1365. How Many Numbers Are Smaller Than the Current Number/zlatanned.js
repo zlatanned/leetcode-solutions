616309a85f1dcf03bcdf5be48090d9c8c5094c14
Akshay Shahi <shahiakshay31@gmail.com>
2021-06-12 22:43:25 +0530

    solved array nos smaller than self prob
    Time: O(nlogn)

---
+++ 1365. How Many Numbers Are Smaller Than the Current Number/zlatanned.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+function smallerNumbersThanCurrent(nums) {
+  const sorted = Array.from(nums).sort((n1, n2) => n2 - n1);
+  const map = new Map(sorted.map((num, index) => [num, nums.length - index - 1]));
+  return nums.map(num => map.get(num));
+}
