1e26efa13d737e3484b623f8feca4643ccf2e090
Akshay Shahi <shahiakshay31@gmail.com>
2021-06-12 23:01:19 +0530

    solved third largest element

---
+++ 414. Third Maximum Number/zlatanned.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var thirdMax = function(nums) {
+    const nums1 = new Set([...nums]);
+    const sorted = Array.from(nums1).sort((a,b) => b-a);
+    return sorted.length < 3 ? sorted[0] : sorted[2];
+};
