3e5d0b7f683c940bd947b998abd04b44062435cd
Akshay Shahi <shahiakshay31@gmail.com>
2021-06-17 20:04:26 +0530

    approach used added

--- 118. Pascal's Triangle/zlatanned.js
+++ 118. Pascal's Triangle/zlatanned.js
@@ -16,5 +16,13 @@ function calculatePascal(numRows) {
 }
 
 /*
-  Approach used: (todo)
+  Approach used: 
+      each row can be calculated by the offset sum of the previous row.
+
+      1 2 1 0
+    + 0 1 2 1
+      -------
+      1 3 3 1
+      
+      
 */
