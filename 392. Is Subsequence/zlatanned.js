b747cc1e5a43127db5a4712fdbfb4e8ad332d364
Akshay Shahi <shahiakshay31@gmail.com>
2021-06-18 23:30:13 +0530

    unnecessary iterations avoided by adding length check on child string as well

--- 392. Is Subsequence/zlatanned.js
+++ 392. Is Subsequence/zlatanned.js
@@ -2,7 +2,7 @@
 
 var isSubsequence = function(s, t) {
     let i = 0, j = 0;
-    while( j < t.length ) {
+    while( j < t.length && i < s.length) {
         if(s[i] === t[j]) {
             i++;
         }
