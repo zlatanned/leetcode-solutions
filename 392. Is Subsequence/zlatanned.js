47473e2053f6d30fd177a8d4128e9ac64f28be4d
Akshay Shahi <shahiakshay31@gmail.com>
2021-06-18 23:28:29 +0530

    made faster than 99.89% solutions

--- 392. Is Subsequence/zlatanned.js
+++ 392. Is Subsequence/zlatanned.js
@@ -2,7 +2,7 @@
 
 var isSubsequence = function(s, t) {
     let i = 0, j = 0;
-    while( j < t.length && i < s.length) {
+    while( j < t.length ) {
         if(s[i] === t[j]) {
             i++;
         }
