3151d53d3c4ade5d3a5534d0382013f55e6a9dd2
Akshay Shahi <shahiakshay31@gmail.com>
2021-06-18 23:24:07 +0530

    solved isSubsequence dp problem using two pointer

---
+++ 392. Is Subsequence/zlatanned.js
@@ -0,0 +1,12 @@
+//Greedy, two pointer solution
+
+var isSubsequence = function(s, t) {
+    let i = 0, j = 0;
+    while( j < t.length && i < s.length) {
+        if(s[i] === t[j]) {
+            i++;
+        }
+        j++;        
+    }
+    return i === s.length ? true: false;
+};
