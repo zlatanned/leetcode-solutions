eae012456cb7414533b97c1f58af7a7119c3e772
Akshay Shahi <shahiakshay31@gmail.com>
2021-08-12 21:25:54 +0530

    Longest Common Prefix using zip - py
    code uploaded with comments

---
+++ 14. Longest Common Prefix/zlatanned.py
@@ -0,0 +1,14 @@
+def longestCommonPrefix(strs):
+    if not(strs): return ""
+
+    pref = ""
+    for s in zip(*strs):
+        # print("----------------", s) => ---------------- ('f', 'f', 'f'), ---------------- ('l', 'l', 'l'), ---------------- ('o', 'o', 'i'),
+        if len(set(s)) != 1: return pref
+        pref += s[0]
+    return pref
+        
+
+# printing resultant values 
+# print ("The zipped result is : ",end="")
+# print (longestCommonPrefix(["flower","flow","flight"]))
