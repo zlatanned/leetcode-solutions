5c252a061294070c9e360f268f486b98293c4624
Akshay Shahi <shahiakshay31@gmail.com>
2021-09-22 23:35:45 +0530

    dsa- day 3 problem solved DP in py

---
+++ 121. Best Time to Buy and Sell Stock/zlatanned.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy = prices[0]
+        profit = 0
+        
+        for price in prices:
+            buy = min(buy, price)
+            profit = max(profit, price - buy)
+            
+        return profit
