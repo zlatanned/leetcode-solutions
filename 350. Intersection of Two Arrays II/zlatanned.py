ad3188408d5fd3a1f69bf4f0a4f7acb7f1dd916b
Akshay Shahi <shahiakshay31@gmail.com>
2021-09-22 23:16:53 +0530

    variable naming improved

--- 350. Intersection of Two Arrays II/zlatanned.py
+++ 350. Intersection of Two Arrays II/zlatanned.py
@@ -1,14 +1,14 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        n1, n2, res = sorted(nums1), sorted(nums2), []
-        p1 = p2 = 0
-        while p1 < len(n1) and p2 < len(n2):
-            if n1[p1] < n2[p2]:
-                p1 += 1
-            elif n2[p2] < n1[p1]:
-                p2 += 1
+        sorted_nums1, sorted_nums2, res = sorted(nums1), sorted(nums2), []
+        pointer1 = pointer2 = 0
+        while pointer1 < len(sorted_nums1) and pointer2 < len(sorted_nums2):
+            if sorted_nums1[pointer1] < sorted_nums2[pointer2]:
+                pointer1 += 1
+            elif sorted_nums2[pointer2] < sorted_nums1[pointer1]:
+                pointer2 += 1
             else:
-                res.append(n1[p1])
-                p1 += 1
-                p2 += 1
+                res.append(sorted_nums1[pointer1])
+                pointer1 += 1
+                pointer2 += 1
         return res
