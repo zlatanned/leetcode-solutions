af1861f940cfe9facfe8c1f1770a32b4e68d44b5
Akshay Shahi <shahiakshay31@gmail.com>
2021-09-25 23:37:33 +0530

    updated problem description for future ref

--- 36. Valid Sudoku/zlatanned.py
+++ 36. Valid Sudoku/zlatanned.py
@@ -1,3 +1,15 @@
+"""
+    Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+    Each row must contain the digits 1-9 without repetition.
+    Each column must contain the digits 1-9 without repetition.
+    Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+    Note:
+
+        1. A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+        2. Only the filled cells need to be validated according to the mentioned rules.
+"""
+
 def isValidSudoku(board):
     return (is_row_valid(board) and
         is_col_valid(board) and
