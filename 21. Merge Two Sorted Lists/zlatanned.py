8e44fc530391c6b65854d2bc50dc6e061757540b
Akshay Shahi <shahiakshay31@gmail.com>
2021-09-27 18:15:49 +0530

    updated problem desc

--- 21. Merge Two Sorted Lists/zlatanned.py
+++ 21. Merge Two Sorted Lists/zlatanned.py
@@ -1,3 +1,10 @@
+"""
+    Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
+        Example 1:
+            Input: l1 = [1,2,4], l2 = [1,3,4]
+            Output: [1,1,2,3,4,4]
+"""
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
