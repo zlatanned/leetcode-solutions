3195832a0f888f685937293f6770482bcf73ee3a
Akshay Shahi <shahiakshay31@gmail.com>
2021-09-27 18:22:36 +0530

    updated problem description

--- 203. Remove Linked List Elements/zlatanned.py
+++ 203. Remove Linked List Elements/zlatanned.py
@@ -1,3 +1,10 @@
+"""
+    Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+        Example 1:
+            Input: head = [1,2,6,3,4,5,6], val = 6
+            Output: [1,2,3,4,5]
+"""
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
