fe8b8ab243052900451a226238290e681e0e42d5
Akshay Shahi <shahiakshay31@gmail.com>
2021-09-27 18:27:14 +0530

    updated problem description

--- 83. Remove Duplicates from Sorted List/zlatanned.py
+++ 83. Remove Duplicates from Sorted List/zlatanned.py
@@ -1,3 +1,10 @@
+"""
+    Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+        Example 1:
+            Input: head = [1,1,2]
+            Output: [1,2]
+"""
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
